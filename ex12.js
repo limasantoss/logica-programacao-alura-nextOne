60417eb92036aaf6e33050641be918217f966f62
Igor Santos <igorlimasantos667@gmail.com>
2025-07-27 01:55:24 -0300

    Para o próximo código, use um novo prompt para perguntar o nome do usuário e armazená-lo em uma variável, pode chamá-la de nome ou adicionar o que desejar .

---
+++ ex12.js
@@ -0,0 +1,8 @@
+//Para o próximo código, use um novo prompt para perguntar o nome do usuário e armazená-lo em uma variável, pode chamá-la de nome ou adicionar o que desejar .
+prompt('Qual o nome correto ?')
+let nome = 'Igor'
+if (nome == 'Igor') {
+    alert('nome correto')
+} else (
+    alert('nome Incorreto')
+)

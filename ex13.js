400d55cc82fcb0bf1b8ce4880d2feac83665641b
Igor Santos <igorlimasantos667@gmail.com>
2025-07-27 01:59:20 -0300

    Peça ao usuário para digitar sua idade usando um prompt e armazene-a na variável idade.

---
+++ ex13.js
@@ -0,0 +1,9 @@
+//Peça ao usuário para digitar sua idade usando um prompt e armazene-a na variável idade.
+
+prompt('Qual a sua idade')
+let idade = 18
+if (idade == 18) {
+    alert('Idade correta ')
+} else {
+    alert('Idade incorreta')
+}

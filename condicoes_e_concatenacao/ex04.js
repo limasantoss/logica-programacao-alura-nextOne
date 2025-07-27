7e35915ddd37088eb95726419d929c01614b652a
Igor Santos <igorlimasantos667@gmail.com>
2025-07-27 18:38:22 -0300

    Faça como eu fiz: console.log

---
+++ condicoes_e_concatenacao/ex04.js
@@ -0,0 +1,16 @@
+let porcentagemDesconto = 0;
+let quantidadeMilhas = 30000
+
+if(quantidadeMilhas){
+    porcentagemDesconto = porcentagemDesconto + 20
+    console.log(quantidadeMilhas)
+    console.log(porcentagemDesconto)
+
+}else{
+    if(quantidadeMilhas > 5000){
+        porcentagemDesconto = porcentagemDesconto + 10
+        console.log(quantidadeMilhas)
+        console.log(porcentagemDesconto)
+    }
+}
+

082e136adfe700f345f96fe7ac1346ba2405b41a
Igor Santos <igorlimasantos667@gmail.com>
2025-07-27 17:41:42 -0300

    concatenação

--- condicoes_e_concatenacao/app.js
+++ condicoes_e_concatenacao/app.js
@@ -1,10 +1,11 @@
 alert('Boas vindas ao jogo do  numero secreto')
-let numero = 5
+let numerSecreto = 4;
+console.log(numerSecreto)
 let chute = Number(prompt('escolha um numero entre 1 a 10'))
-console.log("Resposta correta é  " + numero)
+
 //se chute for igual ao numero secreto 
-if (numero === chute) {
-    alert("Você acertou")
+if (numero == chute) {
+    alert(`Isso ai" você descobriu o número secreto ${numerSecreto} `);
 } else {
-    alert('Você errou')
+    alert('Você errou :( ')
 }

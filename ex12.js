bc7d166c80c285fb66816ab5bbc537e8ff1e9a36
Igor Santos <igorlimasantos667@gmail.com>
2025-07-27 02:24:15 -0300

    Update ex12.js

--- ex12.js
+++ ex12.js
@@ -1,8 +1,8 @@
 //Para o próximo código, use um novo prompt para perguntar o nome do usuário e armazená-lo em uma variável, pode chamá-la de nome ou adicionar o que desejar .
 let nome = prompt('Qual o nome correto ?')
 
-if (nome == 'Igor') {
+if (nome === 'Igor') {
     alert('nome correto')
-} else (
+} else {
     alert('nome Incorreto')
-)
+}

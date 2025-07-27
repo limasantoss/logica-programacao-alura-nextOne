a30f22c813141b83e2c7a4beeac99c184f8726af
Igor Santos <igorlimasantos667@gmail.com>
2025-07-27 18:34:28 -0300

    Trabalhando com condicionais

---
+++ condicoes_e_concatenacao/ex03.js
@@ -0,0 +1,11 @@
+let porcentagemDesconto = 0;
+
+if(quantidadeMilhas){
+    porcentagemDesconto = porcentagemDesconto + 20
+
+}else{
+    if(quantidadeMilhas > 5000){
+        porcentagemDesconto = porcentagemDesconto + 10
+    }
+}
+

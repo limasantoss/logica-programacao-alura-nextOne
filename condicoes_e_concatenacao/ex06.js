ecde33997145ae19993c6b71d18887ab3f3e1cf8
Igor Santos <igorlimasantos667@gmail.com>
2025-07-27 21:41:20 -0300

    Verifique se um número digitado pelo usuário é positivo ou negativo. Mostre um alerta informando

---
+++ condicoes_e_concatenacao/ex06.js
@@ -0,0 +1,8 @@
+let numero = Number(prompt(" Numero é positivo ou negativo"))
+if (numero > 0) {
+    alert(`${numero} é positivo`)
+} else if (numero < 0) {
+    alert(`${numero} é negativo `)
+}else{
+    alert(`${numero} é zero `)
+}

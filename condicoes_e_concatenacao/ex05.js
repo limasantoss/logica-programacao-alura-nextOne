42f36cf9050c5cb4e554b6498ec6bd7a7c20b28a
Igor Santos <igorlimasantos667@gmail.com>
2025-07-27 21:35:04 -0300

    Pergunte ao usuário qual é o dia da semana. Se a resposta for "Sábado" ou "Domingo", mostre "Bom fim de semana!". Caso contrário, mostre "Boa semana!".

---
+++ condicoes_e_concatenacao/ex05.js
@@ -0,0 +1,8 @@
+let diaDaSemana = prompt("Qual dia da semana")
+if (diaDaSemana === "Sabado" || diaDaSemana === "Domingo") {
+    alert(`Bom fim de semana !Hoje é ${diaDaSemana} `)
+    console.log(diaDaSemana)
+} else {
+    alert(`Boa Semana `)
+    console.log(diaDaSemana)
+}

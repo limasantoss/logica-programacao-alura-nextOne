142c671df5d6ca01592fee406a8b36e92c8c60dd
Igor Santos <igorlimasantos667@gmail.com>
2025-07-27 23:39:27 -0300

    enquanto

--- condicoes_e_concatenacao/app.js
+++ condicoes_e_concatenacao/app.js
@@ -1,11 +1,23 @@
 alert('Boas vindas ao jogo do  numero secreto')
 let numeroSecreto = 9;
 console.log(`Resposta correta ${numeroSecreto}`)
-let chute = Number(prompt('escolha um numero entre 1 a 10'))
+let chute
 
-//se chute for igual ao numero secreto 
-if (numeroSecreto == chute) {
-    alert(`Isso ai !!!! você descobriu o número secreto ${numeroSecreto} `);
-} else {
-    alert(`Você errou :( escolheu o numero   ${chute} ,  numero correto é ${numeroSecreto}`)
-}
+//enquanto não for igual ao numero secreto 
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um numero entre 1 a 10')
+    //se chute for igual ao numero secreto 
+    if (chute == numeroSecreto) {
+        alert(`Isso ai !!!! você descobriu o número secreto ${numeroSecreto} `);
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é maior que ${chute}`)
+
+        } else {
+            alert(`O numero secreto é maior que  ${chute}`)
+        }
+
+    }
+    }
+    
+    

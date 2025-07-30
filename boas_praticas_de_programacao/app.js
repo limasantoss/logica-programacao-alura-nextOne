1f0afc7c465fcfbb66f5d74bf6cb0ce25c9d8995
Igor Santos <igorlimasantos667@gmail.com>
2025-07-29 22:10:06 -0300

    Break

---
+++ boas_praticas_de_programacao/app.js
@@ -0,0 +1,29 @@
+
+
+
+
+alert('Boas vindas ao jogo do  numero secreto');
+let numeroSecreto = 9;
+console.log(`Resposta correta ${numeroSecreto}`);
+let chute
+let tentativas = 1
+
+//enquanto chute não for igual ao numero secreto 
+while (chute !== numeroSecreto) {
+    chute = prompt('escolha um numero entre 1 a 10')
+    if (chute == numeroSecreto) {
+        break
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`O numero secreto é menor que ${chute}`)
+        } else {
+            alert(` O número secreto é mairo que ${chute}`)
+        }
+        tentativas++;
+    }
+}
+if(tentativas > 1 ){
+    alert(`isso ai ! você acertou ${numeroSecreto} com ${tentativas} tentivas`)
+}else{
+    alert( `Isso ai! você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa.`)
+}
